66fe895cefd94eb3931e6ba5c6f5fe33e2198124
pappgergelymatyas <pappgergelymatyas@gmail.com>
2024-11-27 16:33:10 +0100

    mentés

--- hw3-pappgergelymatyas-main/homework/nested_lists/nested_lists.py
+++ hw3-pappgergelymatyas-main/homework/nested_lists/nested_lists.py
@@ -1,3 +1,6 @@
 def nested_lists(records):
-    """Write your code here"""
+    grades = sorted(set(record[1] for record in records))
+    second_lowest_grade = grades[1]
+    second_lowest_students = [record[0] for record in records if record[1] == second_lowest_grade]
+    return sorted(second_lowest_students)
     pass
